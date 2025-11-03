328824f2372708e960d61292256436abbaa51204
Bahodirxon <bohodirxon.mahmudoov@gmail.com>
2025-11-03 23:45:48 +0500

    Add Price Function Wrote and Bugs Fixed

--- src/components/Addprice.jsx
+++ src/components/Addprice.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Addprice() {
-  const handleSubmit = (e) => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [price, setPrice] = useState("");
+
+  const navigate = useNavigate();
+  const handleAdd = (e) => {
     e.preventDefault();
+
+    if (!title.trim() || !desc.trim() || !price.trim()) {
+      alert("Please Fill in All Fields !");
+      return;
+    }
+    navigate("/", { state: { title, desc, price } });
+    setTitle("");
+    setDesc("");
+    setPrice("");
   };
+
   return (
     <div>
       <h1 className="text-center mt-5 font-bold">Add Output</h1>
 
-      <form action="" className="form" onSubmit={handleSubmit}>
+      <form action="" className="form" onSubmit={handleAdd}>
         <div className="form-group">
           <label htmlFor="title">Title</label>
           <input
@@ -14,6 +32,8 @@ function Addprice() {
             placeholder="Title..."
             className="input input-primary"
             id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
 
@@ -24,19 +44,26 @@ function Addprice() {
             placeholder="Description..."
             className="input input-primary"
             id="description"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
           />
         </div>
 
         <div className="form-group">
           <label htmlFor="price">Price</label>
           <input
-            type="text"
+            type="number"
             placeholder="Price..."
             className="input input-primary"
             id="price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
           />
+          <span className="usdIcon">$</span>
         </div>
-        <button className="btn btn-soft btn-primary">Primary</button>
+        <button className="btn btn-soft btn-primary" type="submit">
+          Add
+        </button>
       </form>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
+import { useLocation } from "react-router-dom";
+
 import { CiCirclePlus } from "react-icons/ci";
 import { FaUserLarge } from "react-icons/fa6";
 
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const location = useLocation();
+  const newItem = location.state;
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    if (newItem) {
+      setItem((prev) => [...prev, newItem]);
+    }
+  }, [newItem]);
+
   return (
     <div>
       <h2>My Money App</h2>
@@ -17,6 +30,17 @@ function Home() {
           <CiCirclePlus size={50} />
         </button>
       </Link>
+      {item.length === 0 ? (
+        <p>Hozircha hech narsa qo'shilmagan...</p>
+      ) : (
+        item.map((item, index) => (
+          <div key={index}>
+            <h1>{item.title}</h1>
+            <h1>{item.desc}</h1>
+            <h1>{item.price}</h1>
+          </div>
+        ))
+      )}
     </div>
   );
 }
